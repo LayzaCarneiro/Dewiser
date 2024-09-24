2a00e21cc3c31dc77a555544919b710e00b888b3
Layza Carneiro <layza.mrcarneiro@gmail.com>
2024-09-24 10:19:45 -0300

    first commit

---
+++ MakeItGreat/MakeItGreatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItGreatApp.swift
+//  MakeItGreat
+//
+//  Created by Layza Maria Rodrigues Carneiro on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MakeItGreatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
