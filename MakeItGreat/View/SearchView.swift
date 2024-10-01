f3ce7740128cfe54320da4fea9d06db505839d6e
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2024-10-01 08:42:35 -0300

    create search view

---
+++ MakeItGreat/View/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  MakeItGreat
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 30/09/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
