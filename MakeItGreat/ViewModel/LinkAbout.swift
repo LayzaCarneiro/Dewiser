257a42f168249fcd5603ef122f3c6eae14a95171
Layza Carneiro <layza.mrcarneiro@gmail.com>
2024-10-01 21:00:22 -0300

    Refactor to don't conflict with develop

--- MakeItGreat/ViewModel/LinkAbout.swift
+++ MakeItGreat/ViewModel/LinkAbout.swift
@@ -9,22 +9,22 @@ import Foundation
 import SwiftUI
 
 func openLinkedInJR() {
-        if let url = URL(string: "https://www.linkedin.com/in/jo%C3%A3o-roberto-72a05b217/") {
-            UIApplication.shared.open(url)
-        }
+    if let url = URL(string: "https://www.linkedin.com/in/jo%C3%A3o-roberto-72a05b217/") {
+        UIApplication.shared.open(url)
     }
+}
 func openLinkedInHM() {
-        if let url = URL(string: "https://www.linkedin.com/in/hadassa-miranda-525027227/") {
-            UIApplication.shared.open(url)
-        }
+    if let url = URL(string: "https://www.linkedin.com/in/hadassa-miranda-525027227/") {
+        UIApplication.shared.open(url)
     }
+}
 func openLinkedInLC() {
-        if let url = URL(string: "https://www.linkedin.com/in/layzacarneiro/") {
-            UIApplication.shared.open(url)
-        }
+    if let url = URL(string: "https://www.linkedin.com/in/layzacarneiro/") {
+        UIApplication.shared.open(url)
     }
+}
 func openLinkedInMG() {
-        if let url = URL(string: "https://www.linkedin.com/in/melissa-guedes-a05560216/") {
-            UIApplication.shared.open(url)
-        }
+    if let url = URL(string: "https://www.linkedin.com/in/melissa-guedes-a05560216/") {
+        UIApplication.shared.open(url)
     }
+}
