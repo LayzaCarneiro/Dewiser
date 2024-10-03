cc10f8b155c158e1a832d796c163b2817fdb7bda
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2024-10-03 14:38:53 -0300

    added widget

---
+++ MakeItGreat/View/Widget/SimpleWidgetView.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleWidgetView.swift
+//  MakeItGreat
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 03/10/24.
+//
+
+import SwiftUI
+
+struct SimpleWidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimpleWidgetView()
+}
---
+++ MakeItGreat/View/WidgetView.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetView.swift
+//  MakeItGreat
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 03/10/24.
+//
+
+import SwiftUI
+
+struct WidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetView()
+}
