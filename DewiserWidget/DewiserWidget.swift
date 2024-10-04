e43b4543c0d0871f3b39779bd8b601cfcdc5ed1a
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2024-10-04 09:27:33 -0300

    adjusted swift link mistakes

--- DewiserWidget/DewiserWidget.swift
+++ DewiserWidget/DewiserWidget.swift
@@ -12,13 +12,11 @@ struct Provider: AppIntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
     }
-
     func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: configuration)
     }
     func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
         var entries: [SimpleEntry] = []
-
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -26,13 +24,11 @@ struct Provider: AppIntentTimelineProvider {
             let entry = SimpleEntry(date: entryDate, configuration: configuration)
             entries.append(entry)
         }
-
         return Timeline(entries: entries, policy: .atEnd)
     }
-
-//    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
+    //    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
+    //        // Generate a list containing the contexts this widget is relevant in.
+    //    }
 }
 
 struct SimpleEntry: TimelineEntry {
@@ -42,21 +38,22 @@ struct SimpleEntry: TimelineEntry {
 
 struct DewiserWidgetEntryView: View {
     var entry: Provider.Entry
-
     var body: some View {
-        VStack {
-            Text("Make a fast decision")
-                .font(.headline)
-                .bold()
-                .padding()
-                .fontDesign(.rounded)
+        ZStack {
+            Color.purple.opacity(0.5)
+            VStack {
+                Text("Make a fast decision")
+                    .font(.headline)
+                    .bold()
+                    .padding()
+                    .fontDesign(.rounded)
+            }
         }
     }
 }
 
 struct DewiserWidget: Widget {
     let kind: String = "DewiserWidget"
-
     var body: some WidgetConfiguration {
         AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
             DewiserWidgetEntryView(entry: entry)
@@ -73,8 +70,8 @@ extension ConfigurationAppIntent {
     }
     fileprivate static var starEyes: ConfigurationAppIntent {
         let intent = ConfigurationAppIntent()
-       intent.favoriteEmoji = "🤩"
-       return intent
+        intent.favoriteEmoji = "🤩"
+        return intent
     }
 }
 
