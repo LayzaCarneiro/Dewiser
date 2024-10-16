fb82e58734c5e273d1826ea68f612149fceae15a
hadassa <hadvtr@gmail.com>
2024-10-16 11:24:51 -0300

    splashscreen finalizada

--- MakeItGreat/View/SplashScreenView.swift
+++ MakeItGreat/View/SplashScreenView.swift
@@ -9,27 +9,39 @@ import SwiftUI
 import Lottie
 
 struct SplashScreenView: View {
+    @State private var ativado = false
     var body: some View {
-        ZStack {
-            Color.splashscreencolor
-                .ignoresSafeArea()
-            VStack {
-                Spacer()
-                Spacer(minLength: 50)
-                LottieView(animation: .named("animation"))
-                    .playbackMode(.playing(.toProgress(1, loopMode: .playOnce)))
-                    .frame(width: 400, height: 400)
-                Spacer()
-                Text("Dewiser")
-                    .font(.largeTitle)
-                    .fontDesign(.rounded)
-                    .fontWeight(.black)
-                    .foregroundStyle(Color.splashscreentext)
-                    .shadow(color: .black, radius: 2, x: 2, y: 2)
+        if ativado {
+            OnboardingView()
+        } else {
+            ZStack {
+                Color.splashscreencolor
+                    .ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    Spacer(minLength: 200)
+                    LottieView(animation: .named("animation"))
+                        .playbackMode(.playing(.toProgress(1, loopMode: .playOnce)))
+                        .frame(width: 500, height: 500)
+                    Spacer()
+                    Text("Dewiser")
+                        .font(.system(size: 45))
+                        .fontDesign(.rounded)
+                        .fontWeight(.black)
+                        .foregroundStyle(Color.splashscreentext)
+                        .shadow(color: .black, radius: 2, x: 2, y: 2)
+                }
+                .padding()
+                .onAppear(){
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                        self.ativado = true
+                    }
+                }
             }
         }
     }
 }
+        
 #Preview {
     SplashScreenView()
 }
