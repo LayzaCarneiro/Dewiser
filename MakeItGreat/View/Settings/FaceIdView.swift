ee6b8626df308a9ca9d89a363600e5095d228b0f
Layza Carneiro <layza.mrcarneiro@gmail.com>
2024-10-17 19:09:42 -0300

    Adjust color and logic of faceID in settings view

--- MakeItGreat/View/Settings/FaceIdView.swift
+++ MakeItGreat/View/Settings/FaceIdView.swift
@@ -12,26 +12,24 @@ struct FaceIdView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                LinearGradient(stops: [
-                    .init(color: .background, location: 0.90),
-                    .init(color: .background, location: 1.02)
-                ], startPoint: .bottom, endPoint: .top)
-                .ignoresSafeArea()
+                Color.background.ignoresSafeArea()
                 //
                 VStack(alignment: .center) {
                     Image(systemName: "lock.fill")
                         .resizable()
                         .frame(width: 25, height: 36)
-                        .foregroundStyle(Color.textCreateButton)
+                        .foregroundStyle(Color.purpleBackground)
                     //
                     Text(authManager.statusMessage)
                         .bold()
-                        .foregroundStyle(Color.textCreateButton)
+                        .foregroundStyle(Color.purpleBackground)
+                        .fontDesign(.rounded)
                         .padding()
+                        .lineLimit(1)
                     //
                     ZStack {
                         RoundedRectangle(cornerRadius: 8)
-                            .fill(Color("textCreateButton"))
+                            .fill(Color.purpleBackground)
                             .frame(width: 120, height: 30)
                         //
                         Button("Unlock") {
@@ -41,6 +39,8 @@ struct FaceIdView: View {
                                 authManager.statusMessage = "Biometrics is not enabled."
                             }
                         }
+                        .fontWeight(.medium)
+                        .fontDesign(.rounded)
                         .foregroundStyle(Color.white)
                     }
                 }
