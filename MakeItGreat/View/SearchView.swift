f9e377b606117e586204e923dc93d2d6e91d8bbe
Layza Carneiro <layza.mrcarneiro@gmail.com>
2024-10-20 14:15:06 -0300

    Add move item in search view

--- MakeItGreat/View/SearchView.swift
+++ MakeItGreat/View/SearchView.swift
@@ -1,10 +1,90 @@
+////
+////  SearchView.swift
+////  MakeItGreat
+////
+////  Created by Joao Roberto Fernandes Magalhaes on 01/10/24.
+////
 //
-//  SearchView.swift
-//  MakeItGreat
+//import SwiftUI
+//import SwiftData
 //
-//  Created by Joao Roberto Fernandes Magalhaes on 01/10/24.
+//struct SearchView: View {
+//    @State var searchTerm: String = ""
+//    @Query var decisions: [CardModel]
 //
-
+//    var filteredDecisions: [CardModel] {
+//        if searchTerm.isEmpty {
+//            return decisions
+//        } else {
+//            return decisions.filter { $0.title.localizedCaseInsensitiveContains(searchTerm) }
+//        }
+//    }
+//
+//    @State private var selectedDecision: CardModel?
+//
+//    var body: some View {
+//        NavigationStack {
+//            if decisions.isEmpty {
+//                NoSearchView()
+//            } else {
+//                ZStack {
+//                    Color.background.ignoresSafeArea()
+//                    VStack {
+//                        List {
+//                            ForEach(filteredDecisions) { decision in
+//                                HStack {
+//                                    Button {
+//                                        selectedDecision = decision
+//                                    } label: {
+//                                        DecisionCard(card: decision)
+//                                    }
+//                                    .background(
+//                                        NavigationLink(destination: DecisionView(decision: decision)) {
+//                                            EmptyView()
+//                                        }
+//                                            .opacity(0.0)
+//                                            .frame(width: 0, height: 0)
+//                                    )
+//                                }
+//                            }
+//                            .listRowBackground(Color.clear)
+//                            .listRowSeparator(.hidden)
+//                        }
+//                        .listStyle(PlainListStyle())
+//                        .scrollContentBackground(.hidden)
+//                        .background(Color.clear)
+//                    }
+//                }
+//                //
+//                .searchable(text: $searchTerm, placement: .navigationBarDrawer(
+//                    displayMode: .always),
+//                            prompt: "Search your decision cards"
+//                )
+//                var filterDecisions: [CardModel] {
+//                    if searchTerm.isEmpty {
+//                        return decisions
+//                    }
+//                    return decisions.filter {
+//                        $0.title.localizedCaseInsensitiveContains(searchTerm) ||
+//                        (($0.cardDescription?.localizedCaseInsensitiveContains(searchTerm)) != nil)
+//                    }
+//                }
+//            }
+//        }
+//        .navigationTitle("Search")
+//        .navigationBarTitleDisplayMode(.inline)
+//        .onTapGesture {
+//           hideKeyboard()
+//        }
+//    }
+//    func hideKeyboard() {
+//        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+//    }
+//}
+//
+//#Preview {
+//    SearchView()
+//}
 import SwiftUI
 import SwiftData
 
@@ -12,15 +92,16 @@ struct SearchView: View {
     @State var searchTerm: String = ""
     @Query var decisions: [CardModel]
 
+    @State private var selectedDecision: CardModel?
+    @State private var decisionsArray: [CardModel] = []
+
     var filteredDecisions: [CardModel] {
         if searchTerm.isEmpty {
-            return decisions
+            return decisionsArray
         } else {
-            return decisions.filter { $0.title.localizedCaseInsensitiveContains(searchTerm) }
+            return decisionsArray.filter { $0.title.localizedCaseInsensitiveContains(searchTerm) }
         }
     }
-
-    @State private var selectedDecision: CardModel?
 
     var body: some View {
         NavigationStack {
@@ -47,36 +128,37 @@ struct SearchView: View {
                                     )
                                 }
                             }
+                            .onMove { indices, newOffset in
+                                decisionsArray.move(fromOffsets: indices, toOffset: newOffset)
+                            }
                             .listRowBackground(Color.clear)
                             .listRowSeparator(.hidden)
                         }
+                        .scrollIndicators(.hidden)
                         .listStyle(PlainListStyle())
                         .scrollContentBackground(.hidden)
                         .background(Color.clear)
+//                        .toolbar {
+//                            EditButton() // Botão de editar para habilitar a movimentação
+//                        }
                     }
                 }
-                //
                 .searchable(text: $searchTerm, placement: .navigationBarDrawer(
                     displayMode: .always),
                             prompt: "Search your decision cards"
                 )
-                var filterDecisions: [CardModel] {
-                    if searchTerm.isEmpty {
-                        return decisions
-                    }
-                    return decisions.filter {
-                        $0.title.localizedCaseInsensitiveContains(searchTerm) ||
-                        (($0.cardDescription?.localizedCaseInsensitiveContains(searchTerm)) != nil)
-                    }
-                }
             }
         }
         .navigationTitle("Search")
         .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            decisionsArray = decisions // Atualiza o array de decisões
+        }
         .onTapGesture {
            hideKeyboard()
         }
     }
+
     func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
