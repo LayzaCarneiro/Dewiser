c6177f073a03a4d615344772e6631327fc844e87
melissaguedes <melissaguedes13@gmail.com>
2024-10-23 09:26:56 -0300

    Search view

--- MakeItGreat/View/SearchView.swift
+++ MakeItGreat/View/SearchView.swift
@@ -50,6 +50,8 @@ struct SearchView: View {
                             .listRowBackground(Color.clear)
                             .listRowSeparator(.hidden)
                         }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 12)
                         .scrollIndicators(.hidden)
                         .listStyle(PlainListStyle())
                         .scrollContentBackground(.hidden)
