8a73bf7673016fc2c34f2fd04f8b92e45a00fd0d
melissaguedes <melissaguedes13@gmail.com>
2024-10-23 11:43:33 -0300

    Alert when you delete a pro or a con

--- MakeItGreat/View/Components/ItemCard.swift
+++ MakeItGreat/View/Components/ItemCard.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Layza Maria Rodrigues Carneiro on 27/09/24.
 //
-
 import SwiftUI
 import SwiftData
 import Combine
@@ -13,16 +12,17 @@ struct ItemCard: View {
     @Binding var content: String
     let textLimit = 25
     var onDelete: () -> Void
+    @State private var showAlert = false
 
     var body: some View {
         ZStack(alignment: .topTrailing) {
             if content.isEmpty {
-               Text("Add reason")
-                   .foregroundColor(.gray)
-                   .padding(.bottom, 20)
-                   .padding(15)
-                   .frame(width: 149, height: 84, alignment: .leading)
-           }
+                Text("Add reason")
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 20)
+                    .padding(15)
+                    .frame(width: 149, height: 84, alignment: .leading)
+            }
 
             TextEditor(text: $content)
                 .onReceive(Just(content)) { _ in limitText(textLimit) }
@@ -38,11 +38,21 @@ struct ItemCard: View {
                 .opacity(content.isEmpty ? 0.25 : 1)
 
             Button {
-                onDelete()
+                showAlert = true
             } label: {
                 Image(systemName: "xmark")
                     .foregroundColor(.strokecard)
                     .padding(6)
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Confirm Deletion"),
+                    message: Text("Are you sure you want to delete this item?"),
+                    primaryButton: .destructive(Text("Delete")) {
+                        onDelete()
+                    },
+                    secondaryButton: .cancel()
+                )
             }
         }
         .frame(width: 149, height: 84)
